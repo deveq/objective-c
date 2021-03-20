3887892ee8a9e60ded2e8fd8ead51d26e82f2b09
deveq <deveq@kakao.com>
2021-03-21 01:33:41 +0900

    Initial Commit

---
+++ Hello Objective-C/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by Developer on 2021/03/21.
+//
+
+#import <Foundation/Foundation.h>
+int main (int argc, const char *argv[])
+{
+    NSLog (@"Hello, Objective-C!");
+    
+    return (0);
+} // main
